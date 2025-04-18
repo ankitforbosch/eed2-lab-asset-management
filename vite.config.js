f2f7d529aef8a4043693b277a02847cad5431bf8
ankitforbosch <ankitforbosch@gmail.com>
2025-04-18 12:55:50 +0530

    Update vite.config.js for publicDir

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: '.', // Project root
+  publicDir: 'public', // Explicitly set public directory
+  build: {
+    outDir: 'dist', // Output directory for build
+    sourcemap: false
+  }
 });
