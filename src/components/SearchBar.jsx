b54cdb384a1444b2b1b7215d778bb971eedf328e
ankitforbosch <ankitforbosch@gmail.com>
2025-04-18 12:34:51 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+function SearchBar({ setSearchTerm }) {
+  return (
+    <div className="mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search by name or serial number..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
